2d0903d8cbc122ca19c4252300b89abdb5789684
huynhtrucquyen <69337840+huynhtrucquyen@users.noreply.github.com>
2020-08-07 16:06:23 +0700

    Update Exercise3.c

--- Exercise3.c
+++ Exercise3.c
@@ -1,23 +1,50 @@
-/*
-3. Input a string and find the smallest and largest word.
-Ex:
- __________________________________________________________
-| Input: "this is a string with smallest and largest word" |
-| Output: "a" "smallest"                                   |
-|__________________________________________________________|
-*/
 #include <stdio.h>
 #include <stdlib.h>
-
-int ex3(char *str){
-	//your codes here
+#include <string.h>
+int ex3(char *str)
+{
+    char a[100], temp[100];
+    char res_max[100] = "", res_min[100] = "12345677777777777";
+    char words[100][100];
+    int l[100];
+    memset(l, 0, sizeof(l));
+    memset(temp, 0, sizeof(temp));
+    for (int i = 0; i < strlen(str); i++)
+    {
+        a[i] = str[i];
+    }
+    a[strlen(a)]=' ';
+    for (int i = 0; i < strlen(a); i++)
+    {
+        if (a[i] == ' ')
+        {
+            if (temp[0] != ' ')
+            {
+                if (strlen(res_max) < strlen(temp))
+                {
+                    memset(res_max, 0, sizeof(res_max));
+                    for (int j = 0; j < strlen(temp); j++)
+                        res_max[j] = temp[j];
+                }
+                if (strlen(res_min) > strlen(temp))
+                {
+                    memset(res_min, 0, sizeof(res_min));
+                    for (int j = 0; j < strlen(temp); j++)
+                        res_min[j] = temp[j];
+                }
+            }
+            memset(temp, 0, sizeof(temp));
+        }
+        else
+            temp[strlen(temp)] = a[i];
+    }
+    printf("%s %s", res_min, res_max);
 }
-
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	char *str = "This is a string";
-
-	ex3(str);
-		
-	return 0;
+ 
+int main(int argc, char *argv[])
+{
+    char *str = "This is a string";
+    ex3(str);
+ 
+    return 0;
 }
