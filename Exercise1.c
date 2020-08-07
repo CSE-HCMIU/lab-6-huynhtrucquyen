322ba533a6c661d3cf3ee1dcf2f4ebd38738b256
huynhtrucquyen <69337840+huynhtrucquyen@users.noreply.github.com>
2020-08-07 16:54:07 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -5,18 +5,42 @@
 | Output: "one thousand two hundred thirty four" |
 |________________________________________________|
 */
-
+ 
 #include <stdio.h>
 #include <stdlib.h>
-
-int ex1(int testcase){
-	//your codes here
+#include <string.h>
+int ex1(int testcase)
+{
+    char *ones[10] = {"", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine "};
+ 
+    char *tens[10] = {"", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "};
+    int t[5], n = 0;
+    while (testcase > 0)
+    {
+        t[n++] = testcase % 10;
+        testcase = testcase / 10;
+    }
+    for (int i = n-1; i >= 0; i--)
+    {
+        if (i != 1)
+        {
+            printf("%s", ones[t[i]]);
+            if (i == 3)
+                printf("thousand ");
+            if (i == 2)
+                printf("hundred ");
+        }
+        else
+        {
+            printf("%s", tens[t[i]]);
+        }
+    }
 }
-
-int main(int argc, char *argv[]) {
-	int testcase = atoi(argv[1]);
-	
-	ex1(testcase);
-		
-	return 0;
+ 
+int main(int argc, char *argv[])
+{
+    int testcase = atoi(argv[1]);
+    ex1(testcase);
+ 
+    return 0;
 }
